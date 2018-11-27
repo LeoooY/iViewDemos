5eb4c5b550e2a30bb53ae524854e9262eb493953
LeoooY <Leoyy3@163.com>
2018-11-27 09:52:24 +0800

    API CROS

--- src/views/PortalPages/LanguageManage.vue
+++ src/views/PortalPages/LanguageManage.vue
@@ -1,53 +1,64 @@
 <template>
     <div id=LanguageManage>
         <h1>语言管理页面</h1>
+        
         <Row>
             <Col span="20" offset="2">
-                <Col span="12">
-                    <Input v-model="value13" >
-            
-                        <Select v-model="defaultLanguage" slot="prepend" style="width:80px">
-                            <Option value="zh-CN">zh-CN</Option>
-                            <Option value="en-US">en-US</Option>
-                        </Select>
-                        <Button slot="append" icon="ios-search"></Button>
-                    </Input>
-                </Col>
-                <Col span="2" offset="10">
-                    <Button type="primary" width="100px">新增</Button>
-                </Col>
-                
+            <Col span="12">
+            <Input>
+
+            <Select v-model="defaultLanguage" slot="prepend" style="width:80px">
+                <Option v-if="!LangConfig" value="zh-CN">简体中文</Option>
+                <template v-for="L in LangConfig">
+                    <Option value=" L['LangCode'] ">{{ L['DispalyName'] }}</Option>
+                </template> 
+                <!-- <Option  value="zh-CN">简体中文</Option>
+                <Option  value="en-US">English</Option> -->
+            </Select>
+
+
+
+            <Button slot="append" icon="ios-search"></Button>
+            </Input>
+            </Col>
+            <Col span="2" offset="10">
+            <Button type="primary" width="100px" @click="showToken">新增</Button>
             </Col>
 
-           
-                
-            
-            
-            
+            </Col>
+
+
+
+
+
+
         </Row>
-        
+
         <Row>
             <Col span="20" offset="2">
-                <Table id="LanTable" border :columns="columns7" :data="data6"></Table>
-            </Col>    
+            <Table id="LanTable" border :columns="columns" :data="data6"></Table>
+            </Col>
         </Row>
-        
+
         <Page :total="100" />
     </div>
 </template>
 
 <script>
-    
+
     export default {
         name: 'LanguageManage',
-        
+
         data() {
             return {
-                defaultLanguage:'zh-CN',
-                columns7: [
+                
+                UserLoginToken: '',
+                LangConfig:'',
+                defaultLanguage: 'zh-CN',
+                columns: [
                     {
-                        title: 'Name',
-                        key: 'name',
+                        title: 'TableLangId',
+                        key: 'TableLangId',
                         render: (h, params) => {
                             return h('div', [
                                 h('Icon', {
@@ -55,17 +66,17 @@
                                         type: 'md-person'
                                     }
                                 }),
-                                h('strong', params.row.name)
+                                h('strong', params.row.TableLangId)
                             ]);
                         }
                     },
                     {
-                        title: 'Age',
-                        key: 'age'
+                        title: 'TableLangCode',
+                        key: 'TableLangCode'
                     },
                     {
-                        title: 'Address',
-                        key: 'address'
+                        title: 'Value',
+                        key: 'Value'
                     },
                     {
                         title: 'Action',
@@ -105,45 +116,149 @@
                 ],
                 data6: [
                     {
-                        name: 'John Brown',
-                        age: 18,
-                        address: 'New York No. 1 Lake Park'
+                        TableLangId: 'John Brown',
+                        TableLangCode: 18,
+                        Value: 'New York No. 1 Lake Park'
                     },
                     {
-                        name: 'Jim Green',
-                        age: 24,
-                        address: 'London No. 1 Lake Park'
+                        TableLangId: 'Jim Green',
+                        TableLangCode: 24,
+                        Value: 'London No. 1 Lake Park'
                     },
                     {
-                        name: 'Joe Black',
-                        age: 30,
-                        address: 'Sydney No. 1 Lake Park'
+                        TableLangId: 'Joe Black',
+                        TableLangCode: 30,
+                        Value: 'Sydney No. 1 Lake Park'
                     },
                     {
-                        name: 'Jon Snow',
-                        age: 26,
-                        address: 'Ottawa No. 2 Lake Park'
+                        TableLangId: 'Jon Snow',
+                        TableLangCode: 26,
+                        Value: 'Ottawa No. 2 Lake Park'
                     }
-                ]
+                ],
+                data: [],
             }
         },
         methods: {
             show(index) {
                 this.$Modal.info({
                     title: 'User Info',
-                    content: `Name：${this.data6[index].name}<br>Age：${this.data6[index].age}<br>Address：${this.data6[index].address}`
+                    content: `TableLangId: ${this.data6[index].TableLangId}<br>TableLangCode: ${this.data6[index].TableLangCode}<br>Value: ${this.data6[index].Value}`
+                })
+            },
+            showToken() {
+                this.$Modal.info({
+                    title: 'Token',
+                    content: this.UserLoginToken,
                 })
             },
             remove(index) {
+                //判断 提醒是否删除这条数据
                 this.data6.splice(index, 1);
+            },
+
+
+            getEnterToken(isGuest) {
+                fetch('http://bipt.krclouds.com:8090/v1/Token/GetToken/', {
+                    method: "post",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Accept": "application/json",
+                    },
+                    body: JSON.stringify({
+                        "Appid": "f0af81a3-6f36-4419-ab47-18a6c67d3ff2",
+                        "Appsecret": "045e698c-0fb4-4c24-ae7e-e2fb32178b12"
+                    }),
+
+                }).then(result =>  result.json()).then(data => {
+                    console.log('Get Token :',data);
+                    
+                    return data.result.token;
+                }).then(guestToken=>{
+                    isGuest? '' : this.getUserLoginToken(guestToken);
+                })
+                
+            },
+            getUserLoginToken(guestToken) {
+                fetch('http://bipt.krclouds.com:8090/v1/Customer/UserNameLogin/', {
+                    method: "post",
+                    headers: {
+                        "Authorization": guestToken,
+                        "Accept": "application/json",
+                    },
+                    body: JSON.stringify({
+                        "Password": "49ba59abbe56e057",
+                        "PhoneModel": 2,
+                        "PhoneType": "xiaomi6",
+                        "Register": "0000000000000000000",
+                        "RegisterType": 1,
+                        "Uname": "test"
+                    }),
+                }).then(result =>  result.json()).then(data => {
+                    console.log('User Login :',data);
+                    this.UserLoginToken = data.result.token.token;
+                    return data.result.token.token
+                }).then(token=>{
+                    console.log("Login Token :",token);
+                    this.getLangconfig(token);
+                })
+            },
+            getLangconfig(token){
+                fetch('http://bipt.krclouds.com:8090/v1/tb_lang_config/?query=Enabled%3A%201&fields=DispalyName%2CLangCode', {
+                    method: "get",
+                    headers: {
+                        "Authorization": token,
+                    },
+                    
+                }).then(result => result.json()).then(data => {
+                    console.log('Get Lang Config :',data);
+                    this.LangConfig = data.result;
+                    console.log("LangConfig Data :",typeof(this.LangConfig[0]));
+                    console.log("LangConfig Data :",this.LangConfig[1]);
+                    
+                    // return data.result.token.token
+                })
+            },
+            getLangTable(token){
+                fetch('http://bipt.krclouds.com:8090/v1/tb_lang', {
+                    method: "get",
+                    headers: {
+                        "Authorization": token,
+                    },
+                    params:{
+                        "count":"false",
+                        "limit":"20",
+                        "offset":"0",
+                    }, 
+                }).then(result => result.json()).then(data => {
+                    console.log('Get Lang Config :',data);
+                    this.LangConfig = data.result;
+                    console.log("LangTable Data :",this.LangConfig);
+                    // return data.result.token.token
+                    for(let L of this.LangConfig){
+                        console.log(L);
+                    }
+                })
             }
+
+        },
+        created() {
+            let isGuest=false;
+            this.getEnterToken(isGuest);
+            
+            
+
+
+
+
+
         }
     }
 </script>
 
 <style scoped>
-#LanTable{
-    width: 100%;
-    
-}
+    #LanTable {
+        width: 100%;
+
+    }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -6,5 +6,26 @@ module.exports = {
       localeDir: './src/locale',
       enableInSFC: true
     }
+  },
+  devServer:{
+    open:false,
+    host:"localhost",
+    // host:"0.0.0.0" 真机测试
+    // prot:8080,
+    // https:true,
+    // hotOnly:false,
+    
+    
+    // proxy:{
+    //   '/api':{
+    //     target:"http://bipt.krclouds.com:8090/v1/Token/GetToken",
+    //     ws:true,
+    //     changeOrigin:true,
+    //     // pathRewrite{
+
+    //     // },
+    //   }
+    // }
+
   }
 }
